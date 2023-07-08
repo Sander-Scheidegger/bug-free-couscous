75e2e94a5feb43bde217d4539b8b88eacac74da7
unknown <neilscheidegger@gmail.com>
2023-07-08 16:29:50 -0300

    Correção de bugs e adição de criação de arquilo .XLSX

--- main.py
+++ main.py
@@ -13,37 +13,52 @@ import numpy as np
 import pandas as pd
 
 #Lista com lista de palavras:
-nome_lista = ['Sander', 'Pedro', 'André', 'Camila', 'Samuel', 'Carlos','James']
-sobrenome_lista = ['Araújo', 'Moraes', 'da Costa', 'da Silva', 'Paulo', 'bond']
-nacionalidade_lista = ['Acreano', 'amapaense', 'alagoano' 'amazonense', 'baiano', 'cearense', 'brasiliense', 'capixaba', 'goiano', 'maranhese',
-                       'mato-grossense', 'mineiro', 'paraense', 'paraibano', 'paranaense', 'pernambucano', 'piauiense', 'fluminense', 'potiguar', 'gaucho', 'rondoniano', 'roraimense'
-                       'catarinense', 'paulista', 'sergipano', 'tocantinense']
+nome_homem_lista = ['Sander', 'Pedro', 'André', 'Samuel', 'Carlos','James', 'Moisés', 'Felipe', 'Luciano', 'Diego']
+nome_mulher_lista = ['Sandra', 'Patritcia', 'Andreia', 'Samara', 'Carla','Jenifer', 'Monica', 'Thaynara', 'Michele', 'Leticia']
+sobrenome_lista = ['Araújo', 'Moraes', 'da Costa', 'da Silva', 'Paulo', 'Bond', 'Pereira', 'Macedo', 'Pinto']
+nacionalidade_lista = ['acreano', 'amapaense', 'alagoano', 'amazonense', 'baiano', 'cearense', 'brasiliense', 'capixaba', 'goiano', 'maranhese',
+                       'mato-grossense', 'mineiro', 'paraense', 'paraibano', 'paranaense', 'pernambucano', 'piauiense', 'fluminense', 'potiguar', 'gaucho', 'rondoniano', 'roraimense',
+                       'catarinense', 'paulista', 'sergipano', 'tocantinense']#26 itens
 
 #Seleção aleatória
-idade = np.random.randint(1, 101)
+def criarPessoa():
+    idade = np.random.randint(1, 101)
 
-rand = np.random.randint(1,101)
-if rand % 2 == 0:
-    sexo = 'M'
-else:
-    sexo = 'F'
+    rand = np.random.randint(1,101)
+    if rand % 2 == 0:
+        sexo = 'M'
+        rand = np.random.randint(1,10)
+        nome = nome_homem_lista[rand]
+    else:
+        sexo = 'F'
+        rand = np.random.randint(1,10)
+        nome = nome_mulher_lista[rand]
 
-rand = np.random.randint(1,7)
-nome = nome_lista[rand]
+    rand = np.random.randint(1,6)
+    sobrenome = sobrenome_lista[rand]
 
-rand = np.random.randint(1,6)
-sobrenome = sobrenome_lista[rand]
+    rand = np.random.randint(1,25)
+    nacionalidade = nacionalidade_lista[rand]
+    
+    #Formatação do CPF para o formato "xxx.xxx.xxx-xx"
+    rand = np.random.randint(100000000, 1000000000)
+    cpf_cru = str(rand)
+    digito_verificador = str(np.random.randint(10,99))
+    cpf_formatado = "{}.{}.{}-{}".format(cpf_cru[:3], cpf_cru[3:6], cpf_cru[6:9], digito_verificador[:2])
+    return nome, sobrenome, idade, sexo, nacionalidade, cpf_formatado
 
-rand = np.random.randint(1,26)
-nacionalidade = nacionalidade_lista[rand]
 
-#Formatação do CPF para o formato "xxx.xxx.xxx-xx"
-rand = np.random.randint(10000000, 1000000000) # 020.668.371-56
-cpf_cru = str(rand)
-digito_verificador = str(np.random.randint(10,99))
-cpf_formatado = "{}.{}.{}-{}".format(cpf_cru[:3], cpf_cru[3:6], cpf_cru[6:9], digito_verificador[:2])
+qntd = int(input('Digite quantas pessoas deseja criar: '))
+pessoas = []
+for i in range(qntd-1):
+    pessoas.append(criarPessoa())
 
-#conversão para
-pessoa = [nome, sobrenome, idade, sexo, nacionalidade, cpf_formatado]
-df = pd.DataFrame(pessoa)
+#conversão para .CSV
+df = pd.DataFrame(
+    pessoas,
+    columns=["Nome", "Sobrenome", "idade", "Sexo", "Naturalidade", "CPF"]
+    )
 df.to_csv("CSV com Pessoas Aleatórias", header=False, index=False)
+
+# #Criação do arquivo .XLSX
+df.to_excel("Arquivo Excel.xlsx", engine="openpyxl", index=False)
